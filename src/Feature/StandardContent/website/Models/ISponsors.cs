95f25318d8972a355cfb850cd8cab0fb717ab4ea
Mark Manner <mark.manner@possible.com>
2020-02-29 22:54:58 +0100

    Added Sponsors with some test data

---
+++ src/Feature/StandardContent/website/Models/ISponsors.cs
@@ -0,0 +1,10 @@
+﻿namespace Hackathon.Feature.StandardContent.Models
+{
+    using Hackathon.Foundation.ORM.Models;
+    using System.Collections.Generic;
+
+    public interface ISponsors : IGlassBase
+    {
+        IEnumerable<ISponsor> Sponsors { get; set; }
+    }
+}
---
+++ src/Feature/StandardContent/website/ORM/SponsorsMap.cs
@@ -0,0 +1,20 @@
+﻿namespace Hackathon.Feature.StandardContent.ORM
+{
+    using Glass.Mapper.Sc.Maps;
+    using Hackathon.Feature.StandardContent.Models;
+
+    public class SponsorsMap : SitecoreGlassMap<ISponsors>
+    {
+        private const string TemplateId = "{2C829150-5E6E-433A-97EB-CD20BFC65466}";
+
+        public override void Configure()
+        {
+            Map(config =>
+            {
+                config.AutoMap();
+                config.TemplateId(TemplateId);
+                config.Children(t => t.Sponsors);
+            });
+        }
+    }
+}
